b121ae2dc7759344627467d0c86c62298cb7cacf
Micael Campos <micael.campos@gmail.com>
2016-12-04 21:54:17 +0000

    Initial Commit

---
+++ app/src/main/java/com/hangapps/herome/Activities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.hangapps.herome.Activities;
+
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hangapps.herome.Fragments.MainFragment;
+import com.hangapps.herome.R;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        // if fragment does not exist let's create it
+        if( fragment == null){
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+
+
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
+    }
+}
