b3cbca20f5b70aaacdae076059521cbe0bd09030
Micael Campos <micael.campos@gmail.com>
2016-12-04 22:54:40 +0000

    End of lesson 47

--- app/src/main/java/com/hangapps/herome/Fragments/MainFragment.java
+++ app/src/main/java/com/hangapps/herome/Fragments/MainFragment.java
@@ -1,12 +1,14 @@
 package com.hangapps.herome.Fragments;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.hangapps.herome.R;
 
@@ -18,7 +20,7 @@ import com.hangapps.herome.R;
  * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,6 +29,12 @@ public class MainFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button accidentBtn;
+    private Button geneticBtn;
+    private Button bornBtn;
+    private Button chooseBtn;
+
 
     private MainFragmentInteractionListener mListener;
 
@@ -64,8 +72,32 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        accidentBtn = (Button)view.findViewById(R.id.accidentBtn);
+        geneticBtn = (Button)view.findViewById(R.id.geneticBtn);
+        bornBtn = (Button)view.findViewById(R.id.bornBtn);
+        chooseBtn = (Button)view.findViewById(R.id.chooseBtn);
+
+        //disable Choose Button and set Alpha
+        chooseBtn.setEnabled(false);
+        chooseBtn.getBackground().setAlpha(128);
+
+        accidentBtn.setOnClickListener(this);
+        geneticBtn.setOnClickListener(this);
+        bornBtn.setOnClickListener(this);
+
+        chooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -90,6 +122,34 @@ public class MainFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        chooseBtn.setEnabled(true);
+        chooseBtn.getBackground().setAlpha(255);
+
+        // reset the buttons
+        accidentBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.lightning,0,R.drawable.itemunselected,0);
+        geneticBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.atomic,0,R.drawable.itemunselected,0);
+        bornBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.rocket,0,R.drawable.itemunselected,0);
+
+        //grab the view
+        Button btn = (Button)view;
+        int leftDrawable = 0;
+
+        // see witch button is pressed and manage the selected drawable
+        if (btn == accidentBtn){
+            leftDrawable = R.drawable.lightning;
+        } else if (btn == geneticBtn){
+            leftDrawable = R.drawable.atomic;
+        } else if (btn == bornBtn){
+            leftDrawable = R.drawable.rocket;
+        }
+
+        //setting the clicked button
+        btn.setCompoundDrawablesWithIntrinsicBounds(leftDrawable,0,R.drawable.itemselected,0);
+
     }
 
     /**
